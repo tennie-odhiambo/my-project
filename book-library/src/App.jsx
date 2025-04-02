79f6da5981a7017d49df2e0233619eff3bfa2363
unknown <tennie-odhiambo>
2025-04-02 09:36:30 +0300

    Add React Router with book routes

--- book-library/src/App.jsx
+++ book-library/src/App.jsx
@@ -1,14 +1,18 @@
-import SearchBar from "./components/SearchBar"
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import BookPage from './components/BookPage';
 
 function App() {
-
   return (
-    <div>
-      <h1 className="">Book Library</h1>
-      <SearchBar />
-    </div>
-  )
+    <Router>
+      <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/book/:id" element={<BookPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
