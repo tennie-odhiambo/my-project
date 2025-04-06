23ac43eb51e14ab8384d1dec24d1707308645195
unknown <tennie-odhiambo>
2025-04-06 12:37:02 +0300

    Adjust style to darkmode

--- book-library/src/App.jsx
+++ book-library/src/App.jsx
@@ -1,17 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
+import HomePage from './components/HomePage';
 import BookPage from './components/BookPage';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/book/:id" element={<BookPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <Router>
+        <div className="min-h-screen bg-gray-200 dark:bg-gray-800">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/book/:id" element={<BookPage />} />
+          </Routes>
+        </div>
+      </Router>
+      <Footer />
+    </div>
   );
 }
 
